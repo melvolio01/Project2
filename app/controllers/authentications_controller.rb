d55ef986cb36ab35f51636165b628ee7aaa3970f
andy hunt <andyhunt200@hotmail.com>
2016-02-22 19:35:36 +0000

    Authentications controller added

---
+++ app/controllers/authentications_controller.rb
@@ -0,0 +1,15 @@
+get '/register' do
+  @user = User.new
+  erb :'authentications/new'
+end
+
+post '/register' do
+  @user = User.new(params[:user])
+  if @user.save
+    session[:user_id] = @user.user_id
+    flash[:success] = "Thanks for registering!"
+    redirect '/users'
+  else
+    erb :'authentications/new'
+  end
+end
