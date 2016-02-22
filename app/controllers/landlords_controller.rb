55a28963f28e84f5db9d2df9c8dc1ae3a9af2b9a
andy hunt <andyhunt200@hotmail.com>
2016-02-22 22:50:25 +0000

    landlords/properties controllers added

---
+++ app/controllers/landlords_controller.rb
@@ -0,0 +1,53 @@
+# Index
+get '/landlords' do
+  authorize!
+  @landlords = Landlords.all
+  erb :'landlords/index'
+end
+
+# New
+get '/landlords/new' do
+  authorize!
+  @landlord = Landlord.new
+  erb :'landlords/new'
+end
+
+# Create
+post '/landlords' do
+  authorize!
+  @landlord = Landlord.new(params[:landlord])
+  if @landlord.save
+    redirect "/landlords"
+  else
+    erb :'landlords/new'
+  end  
+end
+
+# Show
+get '/landlords/:id' do
+  authorize!
+  @landlord = Landlord.find(params[:id])
+  if @landlord
+    erb :'landlords/show'
+  else
+    redirect "/landlords"
+  end
+end
+
+# Edit
+get "/landlords/:id/edit" do
+  authorize!
+  @landlord = Landlord.find(params[:id])
+  erb :"landlords/edit"
+
+# Update
+put '/landlords/:id' do
+  authorize!
+  @landlord = Landlord.find(params[:id])
+  if @landlord.update(params[:landlord])
+    redirect "/landlords/#{@landlord.id}"
+  else
+    erb :'landlords/show'
+    end
+  end  
+end
---
+++ app/controllers/properties.rb
@@ -0,0 +1,53 @@
+# Index
+get '/properties' do
+  authorize!
+  @properties = Properties.all
+  erb :'properties/index'
+end
+
+# New
+get '/properties/new' do
+  authorize!
+  @property = Property.new
+  erb :'properties/new'
+end
+
+# Create
+post '/properties' do
+  authorize!
+  @property = Property.new(params[:property])
+  if @property.save
+    redirect "/properties"
+  else
+    erb :'properties/new'
+  end  
+end
+
+# Show
+get '/properties/:id' do
+  authorize!
+  @property = Property.find(params[:id])
+  if @property
+    erb :'properties/show'
+  else
+    redirect "/properties"
+  end
+end
+
+# Edit
+get "/properties/:id/edit" do
+  authorize!
+  @property = Property.find(params[:id])
+  erb :"properties/edit"
+
+# Update
+put '/properties/:id' do
+  authorize!
+  @property = Property.find(params[:id])
+  if @property.update(params[:property])
+    redirect "/properties/#{@property.id}"
+  else
+    erb :'properties/show'
+    end
+  end  
+end
