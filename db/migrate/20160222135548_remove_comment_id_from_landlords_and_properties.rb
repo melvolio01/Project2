61cc4560946390f946899efc519b2d3ac3abaf14
andy hunt <andyhunt200@hotmail.com>
2016-02-26 11:07:06 +0000

    Modified migration for heroku

--- db/migrate/20160222135548_remove_comment_id_from_landlords_and_properties.rb
+++ db/migrate/20160222135548_remove_comment_id_from_landlords_and_properties.rb
@@ -1,6 +1,5 @@
 class RemoveCommentIdFromLandlordsAndProperties < ActiveRecord::Migration
   def change
-    remove_column :landlords, :comment_id
     remove_column :properties, :comment_id
   end
 end
